e83d20e63b94b95464ce024fa544f5da3b385152
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2023-05-27 23:55:21 +0700

    Task18

--- Python_Seminars/Homework3/Task18.py
+++ Python_Seminars/Homework3/Task18.py
@@ -11,3 +11,48 @@
 6
 -> 5"
 """
+import random
+
+# Вариант №1:
+n = int(
+    input("Введите кол-во элементов в массиве: N = "))
+
+a = list(map(int, input(
+    f'Ввведите через пробел {n} целых чисел(а) для создания массива А = [1..N]: ').split()))
+print(a)
+
+if len(a) != n:
+    print("ошибка!")
+else:
+    x = int(input("Введите искомое число: Х = "))
+
+    k = 0
+    min = abs(x - a[0])
+
+    for i in range(len(a[:])):
+        count = abs(x - a[i])
+        if count < min:
+            min = count
+            k = i
+print(
+    f"Самый близкий по величине элемент к заданному числу Х = {x} в массиве A, это элемент c индексом {k}: А[{k}] = {a[k]} ")
+
+# Вариант №2:
+n = int(input("Введите кол-во элементов в массиве: N = "))
+
+a = [random.randint(1, n) for i in range(n)]
+print(f'A = {a} ')
+
+x = int(input("Введите искомое число: Х = "))
+
+k = 0
+min = abs(x - a[0])
+
+for i in range(1, n):
+    count = abs(x - a[i])
+    if count < min:
+        min = count
+        k = i
+
+print(
+    f"Самый близкий по величине элемент к заданному числу Х = {x} в массиве A, это элемент c индексом {k}: А[{k}] = {a[k]} ")
