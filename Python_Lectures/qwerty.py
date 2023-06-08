b995e21c86afa48ed09b34b6b5ad9557a3c8ea21
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2023-06-08 18:05:24 +0700

    Lecture4

--- Python_Lectures/qwerty.py
+++ Python_Lectures/qwerty.py
@@ -1,13 +1,5 @@
-def merge_sort(nums):
-    if len(nums) > 1:
-        mid = len(nums) // 2
-        left = nums[:mid]
-        right = nums[mid:]
-        merge_sort(left)
-        merge_sort(right)
-        i = j = k = 0
 
-
-nums = [38, 27, 43, 3, 9, 82, 10]
-merge_sort(nums)
-print(nums)  # [3, 9, 10, 27, 38, 43, 82]
+list_1 = [x for x in range (1, 20)]
+y = 2
+list_1 = list(map(lambda x: x ** 2, list_1))
+print(list_1)
