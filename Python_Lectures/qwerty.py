76180a31b40f9c05755c4f94b074803707a9dd76
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2023-06-18 17:58:01 +0700

    _

--- Python_Lectures/qwerty.py
+++
@@ -1,9 +0,0 @@
-def find_farthest_orbit(list_of_orbits):
-    list_of_elliptical_orbits = [i for i in list_of_orbits if i[0] != i[1]]
-    list_of_areas = [(i[0] * i[1]) for i in list_of_elliptical_orbits]
-    max_area_index = list_of_areas.index(max(list_of_areas))
-    return list_of_elliptical_orbits[max_area_index]
-
-
-orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-print(*find_farthest_orbit(orbits))
---
+++ s.py
@@ -0,0 +1,4 @@
+a = {1, 1, 2, 2}
+print(a)
+a.copy()
+print(a)
